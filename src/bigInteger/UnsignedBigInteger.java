a2c53496a4b258ebf4336e8277bf27d0f14b439f
sevenineone <alexeyka791@gmail.com>
2020-03-04 23:32:45 +0200

    Initial commit

---
+++ src/bigInteger/UnsignedBigInteger.java
@@ -0,0 +1,202 @@
+package bigInteger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class UnsignedBigInteger {
+    private List<Integer> number = new ArrayList<>();
+
+    public UnsignedBigInteger(String number) {
+        if (!number.matches("[0-9]*")) throw new IllegalArgumentException("Wrong format, it must be string");
+        for (int i = number.length() - 1; i >= 0; i--) {
+            this.number.add(Character.getNumericValue(number.charAt(i)));
+        }
+    }
+
+    private void setUBIDigit(int digit) {
+        this.number.add(digit);
+    }
+
+    private void setUBIDigit(int i, int digit) {
+        this.number.add(i, digit);
+    }
+
+    private void trimUBI(int i) {
+        this.number = this.number.subList(0, i);
+    }
+
+    private void replaceUBDDigit(int i, int digit) {
+        if (i >= this.number.size()) this.number.add(i, digit);
+        this.number.set(i, digit);
+    }
+
+    private int getUBISize() {
+        return this.number.size();
+    }
+
+    private int getUBIDigit(int i) {
+        if (i < this.number.size())
+            return this.number.get(i);
+        else return 0;
+    }
+
+    public void printUnsignedBigInteger() {
+        for (int i = this.number.size() - 1; i >= 0; i--) {
+            System.out.print(this.number.get(i));
+        }
+    }
+
+    public int compare(UnsignedBigInteger other) {
+        if (number.size() < other.number.size()) return 1;
+        else if (number.size() > other.number.size()) return -1;
+        else {
+            for (int i = number.size() - 1; i >= 0; i--)
+                if (number.get(i) > other.number.get(i)) return -1;
+                else if (number.get(i) < other.number.get(i)) return 1;
+            return 0;
+        }
+    }
+
+    public UnsignedBigInteger sum(UnsignedBigInteger other) {
+        int c = 0;
+        int i;
+        UnsignedBigInteger ans = new UnsignedBigInteger("");
+        if (Math.max(number.size(), other.number.size()) == 1 && number.get(0) + other.number.get(0) < 10) {
+            ans.setUBIDigit(number.get(0) + other.number.get(0));
+        } else {
+            for (i = 0; i < Math.max(number.size(), Math.max(other.number.size(), 2)); i++) {
+                int a, b;
+                if (i >= number.size()) { // getOrDefault ???
+                    a = 0;
+                } else a = number.get(i);
+                if (i >= other.number.size()) {
+                    b = 0;
+                } else b = other.number.get(i);
+
+                c += a + b;
+                ans.setUBIDigit(c % 10);
+                c /= 10;
+            }
+        }
+        if (c > 0)
+            ans.setUBIDigit(c);
+        return ans;
+    }
+
+    public UnsignedBigInteger sub(UnsignedBigInteger other) {
+        if (this.compare(other) == 1) throw new IllegalArgumentException("A must be bigger then B!");
+        UnsignedBigInteger C = new UnsignedBigInteger("");
+        if (this.compare(other) == 0) {
+            C.setUBIDigit(0);
+            return C;
+        }
+        int c = 0;
+        for (int i = 0; i < number.size(); i++) {
+            try {
+                c += number.get(i) - other.number.get(i);
+            } catch (IndexOutOfBoundsException e) {
+                c += number.get(i);
+            }
+            C.setUBIDigit((c + 10) % 10);
+            if (c < 0) c = -1;
+            else c = 0;
+        }
+        int i = C.getUBISize() - 1;
+        while (C.getUBIDigit(i) == 0 && i != 0) {
+            i--;
+        }
+        C.trimUBI(i + 1);
+        return C;
+    }
+
+    public UnsignedBigInteger mul(UnsignedBigInteger other) {
+        int remainder = 0;
+        int k = 0;
+        UnsignedBigInteger C = new UnsignedBigInteger("");
+        for (int i = 0; i < number.size(); i++) {
+            remainder = 0;
+            for (int j = 0; j < other.number.size(); j++) {
+                remainder = number.get(i) * other.number.get(j) + C.getUBIDigit(k + j) + remainder;
+                C.replaceUBDDigit(k + j, remainder % 10);
+                remainder /= 10;
+            }
+            k++;
+        }
+        if (remainder > 0) C.setUBIDigit(remainder);
+        return C;
+    }
+
+    public UnsignedBigInteger div(UnsignedBigInteger other) {
+        if (other.number.size() == 1 && other.number.get(0) == 0)
+            throw new IllegalArgumentException("Maths error: Divider can't be zero");
+        UnsignedBigInteger ans = new UnsignedBigInteger("");
+        if (this.compare(other) == 1){
+            ans.setUBIDigit(0);
+            return ans;
+        }
+        UnsignedBigInteger divA = new UnsignedBigInteger("");
+        UnsignedBigInteger divB;
+        UnsignedBigInteger space = new UnsignedBigInteger("");
+        int digit;
+        if (number.size() == 1 && number.get(0) == 0) {
+            ans.setUBIDigit(0);
+            return ans;
+        }
+        for (int i = number.size() - 1; i >= 0; i--) {
+            divA.setUBIDigit(0, number.get(i));
+            if (divA.compare(other) == 1) {
+                if (ans.getUBISize() > 0) {
+                    ans.setUBIDigit(0, 0);
+                }
+            } else if (divA.compare(other) == 0) {
+                divA = space;
+                ans.setUBIDigit(0, 1);
+            } else {
+                digit = 0;
+                divB = other;
+                while (divA.compare(divB) < 1) {
+
+                    divB = divB.sum(other);
+                    digit++;
+                }
+                ans.setUBIDigit(0, digit);
+                divB = divB.sub(other);
+                divA = divA.sub(divB);
+                if (divA.getUBIDigit(0) == 0 && divA.getUBISize() == 1) divA = space;
+            }
+        }
+        return ans;
+    }
+
+    public UnsignedBigInteger remainder
+            (UnsignedBigInteger other) {
+        UnsignedBigInteger ans = new UnsignedBigInteger("");
+        if (this.compare(other) == 1){
+            ans.setUBIDigit(0);
+            return ans;
+        }
+        return this.sub(other.mul(this.div(other)));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UnsignedBigInteger that = (UnsignedBigInteger) o;
+        return Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return "bigInteger.UnsignedBigInteger{" +
+                "number=" + number +
+                '}';
+    }
+
+}
---
+++ test/bigInteger/UnsignedBigIntegerTests.java
@@ -0,0 +1,114 @@
+package bigInteger;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UnsignedBigIntegerTests {
+    private UnsignedBigInteger
+            a1 = new UnsignedBigInteger("4"), b1 = new UnsignedBigInteger("4"),
+            a2 = new UnsignedBigInteger("123"), b2 = new UnsignedBigInteger("3"),
+            a3 = new UnsignedBigInteger("12"), b3 = new UnsignedBigInteger("34"),
+            a4 = new UnsignedBigInteger("153513513413413"), b4 = new UnsignedBigInteger("1351351"),
+            a5 = new UnsignedBigInteger("1100"), b5 = new UnsignedBigInteger("10"),
+            a6 = new UnsignedBigInteger("2"), b6 = new UnsignedBigInteger("8"),
+            a7 = new UnsignedBigInteger("125"), b7 = new UnsignedBigInteger("3");
+
+
+    @Test
+    public void compareTest() {
+        assertEquals(0, a1.compare(b1));
+        assertEquals(-1, a2.compare(b2));
+        assertEquals(1, a3.compare(b3));
+        assertEquals(-1, a4.compare(b4));
+        assertEquals(-1, a5.compare(b5));
+        assertEquals(1, a6.compare(b6));
+    }
+
+    @Test
+    public void sumUnsignedBigIntegerTest() {
+        UnsignedBigInteger
+                ans1 = new UnsignedBigInteger("8"),
+                ans2 = new UnsignedBigInteger("126"),
+                ans3 = new UnsignedBigInteger("46"),
+                ans4 = new UnsignedBigInteger("153513514764764"),
+                ans5 = new UnsignedBigInteger("1110"),
+                ans6 = new UnsignedBigInteger("10");
+        assertEquals(ans1, a1.sum(b1));
+        assertEquals(ans2, a2.sum(b2));
+        assertEquals(ans3, a3.sum(b3));
+        assertEquals(ans4, a4.sum(b4));
+        assertEquals(ans5, a5.sum(b5));
+        assertEquals(ans6, a6.sum(b6));
+    }
+
+    @Test
+    public void subUnsignedBigIntegerTest() {
+        UnsignedBigInteger
+                ans1 = new UnsignedBigInteger("0"),
+                ans2 = new UnsignedBigInteger("120"),
+                ans4 = new UnsignedBigInteger("153513512062062"),
+                ans5 = new UnsignedBigInteger("1090");
+        assertEquals(ans1, a1.sub(b1));
+        assertEquals(ans2, a2.sub(b2));
+        assertEquals(ans4, a4.sub(b4));
+        assertEquals(ans5, a5.sub(b5));
+    }
+
+
+    @Test
+    public void mulUnsignedBigIntegerTest() {
+        UnsignedBigInteger
+                ans1 = new UnsignedBigInteger("16"),
+                ans2 = new UnsignedBigInteger("369"),
+                ans3 = new UnsignedBigInteger("408"),
+                ans4 = new UnsignedBigInteger("207450639864729070963"),
+                ans5 = new UnsignedBigInteger("11000"),
+                ans6 = new UnsignedBigInteger("16");
+        assertEquals(ans1, a1.mul(b1));
+        assertEquals(ans2, a2.mul(b2));
+        assertEquals(ans3, a3.mul(b3));
+        assertEquals(ans4, a4.mul(b4));
+        assertEquals(ans5, a5.mul(b5));
+        assertEquals(ans6, a6.mul(b6));
+    }
+
+    @Test
+    public void divUnsignedBigIntegerTest() {
+        UnsignedBigInteger
+                ans1 = new UnsignedBigInteger("1"),
+                ans2 = new UnsignedBigInteger("41"),
+                ans3 = new UnsignedBigInteger("0"),
+                ans4 = new UnsignedBigInteger("113600029"),
+                ans5 = new UnsignedBigInteger("110"),
+                ans6 = new UnsignedBigInteger("0");
+        assertEquals(ans1, a1.div(b1));
+        assertEquals(ans2, a2.div(b2));
+        assertEquals(ans3, a3.div(b3));
+        assertEquals(ans4, a4.div(b4));
+        assertEquals(ans5, a5.div(b5));
+        assertEquals(ans6, a6.div(b6));
+    }
+
+    @Test
+    public void remainderUnsignedBigIntegerTest() {
+        UnsignedBigInteger
+                ans1 = new UnsignedBigInteger("0"),
+                ans2 = new UnsignedBigInteger("0"),
+                ans3 = new UnsignedBigInteger("0"),
+                ans4 = new UnsignedBigInteger("624234"),
+                ans5 = new UnsignedBigInteger("0"),
+                ans6 = new UnsignedBigInteger("0"),
+                ans7 = new UnsignedBigInteger("2");
+        assertEquals(ans1, a1.remainder(b1));
+        assertEquals(ans2, a2.remainder(b2));
+        assertEquals(ans3, a3.remainder(b3));
+        assertEquals(ans4, a4.remainder(b4));
+        assertEquals(ans5, a5.remainder(b5));
+        assertEquals(ans6, a6.remainder(b6));
+        assertEquals(ans7, a7.remainder(b7));
+    }
+
+}
